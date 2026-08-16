44ee2b86e6dc483a1f58a1c6a29f08249e3b591f
anttiluode <67085641+anttiluode@users.noreply.github.com>
2026-08-16 18:54:38 +0300

    Expose DifferentMachine primitives

---
+++ different_machine/__init__.py
@@ -0,0 +1,17 @@
+from .core import (
+    InputEvent,
+    PromotionEvent,
+    MachineSpec,
+    EventMachine,
+    ClockedMachine,
+    generate_events,
+)
+
+__all__ = [
+    "InputEvent",
+    "PromotionEvent",
+    "MachineSpec",
+    "EventMachine",
+    "ClockedMachine",
+    "generate_events",
+]
